48ddf4d2eebf899e724ada0749204be8691f1fe0
Rostyslav Synenko <rostyslav.synenko@gmail.com>
2020-06-10 21:58:57 +0300

    Updates

---
+++ precache-manifest.62036cea1c5b7070f9964b30abedd2ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef6af999bc0f841caa69d761a4ac60fd",
+    "url": "/quotes-generator/index.html"
+  },
+  {
+    "revision": "bc8a9b808485d73635ab",
+    "url": "/quotes-generator/static/css/main.ceadec95.chunk.css"
+  },
+  {
+    "revision": "18dd857dfeb6f95fdb22",
+    "url": "/quotes-generator/static/js/2.fd8fc580.chunk.js"
+  },
+  {
+    "revision": "bc8a9b808485d73635ab",
+    "url": "/quotes-generator/static/js/main.2273e267.chunk.js"
+  },
+  {
+    "revision": "c49d1fd17990206777d9",
+    "url": "/quotes-generator/static/js/runtime~main.07755177.js"
+  }
+]);
